e861cd696bf7ce43737bdb676189860fd7070f69
sramanovich <sramanovich@gmail.com>
2017-02-11 22:47:22 +0300

    add context menu

--- app/src/main/java/net/sramanovich/fitnessday/ExercisesActivity.java
+++ app/src/main/java/net/sramanovich/fitnessday/ExercisesActivity.java
@@ -1,12 +1,19 @@
 package net.sramanovich.fitnessday;
 
 import android.app.Activity;
+import android.support.v7.app.ActionBar;
 import android.database.Cursor;
 import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+//import android.view.View;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
+//import android.widget.Toast;
 
-public class ExercisesActivity extends Activity {
+public class ExercisesActivity extends AppCompatActivity {
 
     private CursorAdapter cursorAdapter;
 
@@ -24,5 +31,19 @@ public class ExercisesActivity extends Activity {
         if( cursorAdapter!= null ) {
             lvData.setAdapter(cursorAdapter);
         }
+
+        /*ActionBar actionBar = getSupportActionBar();
+        if( actionBar != null ) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }*/
+
+        /*FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ExercisesActivity.this, "Would you like a coffee?", Toast.LENGTH_SHORT).show();
+            }
+        });*/
     }
 }
--- app/src/main/java/net/sramanovich/fitnessday/MainActivity.java
+++ app/src/main/java/net/sramanovich/fitnessday/MainActivity.java
@@ -3,12 +3,15 @@ package net.sramanovich.fitnessday;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar toolbar;
 
@@ -36,12 +39,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
     private void initToolbar() {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
+                System.out.println(item.toString());
                 return false;
             }
         });
